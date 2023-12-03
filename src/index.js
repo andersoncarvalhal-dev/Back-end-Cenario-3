8155c7d60db20c9d7ed0b9c5ab043d7998353f95
andersoncarvalhal-dev <andersonpimenta10@hotmail.com>
2023-12-03 16:41:31 -0300

    Rotas

--- src/index.js
+++ src/index.js
@@ -1,49 +1,43 @@
 //importações necessárias para o projeto
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const app = express();
+const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express();
-const prisma = new PrismaClient();
-const port = process.env.PORT || 10000;
-
-//configurações para o servidor
+//configuração de json e cors
 app.use(bodyParser.json());
 app.use(cors());
 
-//rota para criar um item
-//Rotas
+//rota que lista todos os usuários cadastrados
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  if (users.length > 0) return res.status(200).send(users);
-  return res.send("No users found");
 });
 
+//rota que cadastra um usuário
 app.post("/user", async (req, res) => {
-  const data = req.body;
-  await prisma.user.create({
-    data: {
-      nome: data.nome,
-    },
-  });
-  return res.sendStatus(201);
 });
 
-//rota para buscar um usuário pelo nome
-app.get("/user/:name", async (req, res) => {
-  const nome = req.params.name;
-  const user = await prisma.user.findMany({
-    where: {
-      nome: nome,
-    },
-  });
-  if (user.length > 0) return res.status(200).send(user);
-  return res.send("No user found");
+//rota que apaga um usuário, passando o id
+app.delete("/user/:id", async (req, res) => {
 });
 
+//rota que atualiza um usuário, pelo id
+app.put("/user/:id", async (req, res) => {
+});
+
+//rota que lista usuários que contenham o nome específico
+app.get("/users/:name", async (req, res) => {
+});
+
+//rota que lista um usuário pelo id
+app.get("/user/:id", async (req, res) => {
+});
 
 // Inicie o servidor na porta especificada
-app.listen(port, () => {
-  console.log(`Servidor Express rodando na porta ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
+
+module.exports = { app, server };
