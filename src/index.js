10e19918e9df25a161b385168e072fa2d833b801
andersoncarvalhal-dev <andersonpimenta10@hotmail.com>
2023-12-04 19:59:00 -0300

    new2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
